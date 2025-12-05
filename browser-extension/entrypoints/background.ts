a18f7725c901c8b3a7733a7bbbd11698ab080a99
yamachu <yamachu.dev@gmail.com>
2025-12-05 19:26:05 +0900

    sidePanelを試したいので、そのままrename

--- browser-extension/entrypoints/background.ts
+++ browser-extension/entrypoints/background.ts
@@ -1,3 +1,5 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log("Hello background!", { id: browser.runtime.id });
+
+  browser.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 });
